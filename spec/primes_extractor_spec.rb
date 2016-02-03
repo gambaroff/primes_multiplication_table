3fd8d788cd2fa96b51e5447d031dc0c9b3ba0920
gambaroff <stgambar@gambaroff.com>
2016-02-02 21:45:35 -0800

    Clean up rspec assertions

--- spec/primes_extractor_spec.rb
+++ spec/primes_extractor_spec.rb
@@ -2,11 +2,11 @@ require 'primes_extractor'
 require 'rspec'
 
 RSpec.describe PrimeExtractor do
-  let(:extract_primes) { PrimeExtractor}
+  let(:extract_primes) { PrimeExtractor }
   let(:num) { 10 }
 
   describe '.prime?' do
-    it 'takes a maximum returns an array of primes' do
+    it 'takes an integer and checks if it is a prime' do
       expect(extract_primes.prime?(7)).to eq true
       expect(extract_primes.prime?(29)).to eq true
       expect(extract_primes.prime?(4)).to eq false
@@ -15,9 +15,7 @@ RSpec.describe PrimeExtractor do
   end
 
   describe '.primes' do
-
-
-    it 'takes a maximum returns an array of primes' do
+    it 'takes N and returns an array of N number of primes' do
       expect(extract_primes.primes(num)).to eq [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
     end
   end
